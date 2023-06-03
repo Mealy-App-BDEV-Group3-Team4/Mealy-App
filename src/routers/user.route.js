d53d9e934b44ada5baac6ff13881e5174140eba0
BENITA OJUGO EKENE-OKIKERE <dmnion4.glowd@gmail.com>
2023-06-03 16:27:34 +0100

    updated

--- src/routers/user.route.js
+++ src/routers/user.route.js
@@ -8,13 +8,13 @@ router.post("/signUp", tryCatchHandler( UserController.userSignUp) )
 
 router.post("/login", tryCatchHandler( UserController.userLogin) )
 
-router.patch('/:id', tryCatchHandler( UserController.userUpdate) )
+// router.patch('/:id', tryCatchHandler( UserController.userUpdate) )
 
-router.post('/reset-password', tryCatchHandler( UserController.forgotPassword) )
+router.post('/forgotPassword', tryCatchHandler( UserController.forgotPassword) )
 
-router.post('/password-reset/:userId/:token', tryCatchHandler( UserController.resetUserPassword) )
+// router.post('/password-reset/:userId/:token', tryCatchHandler( UserController.resetUserPassword) )
 
-router.get('find/:id', tryCatchHandler( UserController.findUser) )
+// router.get('find/:id', tryCatchHandler( UserController.findUser) )
 
 router.post('/logout', tryCatchHandler( UserController.logOut) )
 
